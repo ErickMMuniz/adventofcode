0936b5b41775c680f261f4ff32bf578470c246a7
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2023-12-02 16:36:35 -0600

    day 1

---
+++ src/2023/day01.py
@@ -0,0 +1,59 @@
+import re
+import functools
+import os
+from typing import List, Text
+import numpy as np
+
+# AoC => AdventOfCode
+DAY_AoC = os.path.basename(__file__).split(".")[0]
+INPUT_AoC = os.path.join(os.getcwd(), "src", "2023", "inputs", "{}.txt".format(DAY_AoC))
+
+VALID_NUMBERS = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def extract_head_and_tail_digist(text: Text) -> int:
+    matches: List[int] = re.findall("\d", text)
+    head, last = matches[0], matches[-1]
+    return int("".join([head, last]))
+
+
+def to_number(maybe_int: Text) -> int:
+    if maybe_int in VALID_NUMBERS:
+        return str(VALID_NUMBERS[maybe_int])
+    else:
+        return maybe_int
+
+
+def extract_head_and_last_digit_with_words(text: Text) -> int:
+    pattern = "({}|\d)".format("|".join(VALID_NUMBERS.keys()))
+    overlaping_pattern = "(?={})".format(pattern)
+
+    matches: List[int] = re.findall(overlaping_pattern, text)
+
+    head, last = matches[0], matches[-1]
+
+    return int("".join([to_number(head), to_number(last)]))
+
+
+if __name__ == "__main__":
+    inputs = np.loadtxt(INPUT_AoC, dtype=str)
+
+    f_1 = np.vectorize(extract_head_and_tail_digist)
+    result = f_1(inputs).sum()
+
+    assert 54708 == result
+
+    f_2 = np.vectorize(extract_head_and_last_digit_with_words)
+    result = f_2(inputs).sum()
+
+    assert 54087 == result
