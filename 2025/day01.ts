f6dd740581ce520deeb3b23a8c1bcd66e09d424d
Erick Muñiz Morales <emm98p2@gmail.com>
2025-12-02 00:53:42 -0600

    better code using tsconfig.json file

--- 2025/day01.ts
+++ 2025/day01.ts
@@ -2,19 +2,5 @@ import * as fs from 'fs';
 
 const input_01 : string = 'input/day01_01.txt';
 
-try {
-  // Synchronous reading
-  const fileContent: string = fs.readFileSync(input_01, 'utf-8');
-  console.log('Synchronous file content:', fileContent);
+const fileContent: string = fs.readFileSync(input_01, 'utf-8');
 
-  // Asynchronous reading
-  fs.readFile(input_01, 'utf-8', (err, data) => {
-    if (err) {
-      console.error('Error reading file asynchronously:', err);
-      return;
-    }
-    console.log('Asynchronous file content:', data);
-  });
-} catch (error) {
-  console.error('Error reading file synchronously:', error);
-}
