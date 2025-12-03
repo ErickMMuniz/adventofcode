cebbc9750e2ea3e032a3c255b350f893648b4e17
Erick Muñiz Morales <emm98p2@gmail.com>
2025-12-03 01:37:38 -0600

    first star

--- 2025/dist/day01.js
+++ 2025/dist/day01.js
@@ -36,3 +36,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const input_01 = 'input/day01_01.txt';
 const fileContent = fs.readFileSync(input_01, 'utf-8');
+// First start
+// - The dial starts by pointing at 50.
+// - A rotation starts with an L or R which indicates whether the rotation should be to the left (toward lower numbers) or to the right (toward higher numbers).
+// - Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
+function countDialIsZeroAfterRotations(lines) {
+    let dialCounting = 0;
+    let currentState = 50;
+    lines.split('\n').forEach(line => {
+        const direction = line[0];
+        const steps = parseInt(line.slice(1));
+        if (direction === 'R') {
+            currentState = (currentState + steps) % 100;
+        }
+        else if (direction === 'L') {
+            currentState = (currentState - steps + 100) % 100;
+        }
+        if (currentState === 0) {
+            dialCounting++;
+        }
+    });
+    return dialCounting;
+}
+console.log(countDialIsZeroAfterRotations(fileContent));
